1e3a3044ef14d1d3c66fe8bfe5245ffc0eeb66e2
ymc <ymc@cheok.com>
2019-06-03 14:33:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## lazyani realize dom lazy loading add some animate
+
+- npm 
+  npm install lazyani
+
+- yarn 
+  yarn add lazyani
+
--- index.js
+++ index.js
@@ -1,39 +1,52 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.default = void 0;
+
 /**
  * Vue DOM懒动画
  *
  * DOM 随滚动进度，在可视区显示情况渲染
  *
  */
-export default (Vue, options = {}) => {
+var _default = function _default(Vue) {
+  var options =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
   if (!Array.prototype.remove) {
     Array.prototype.remove = function(item) {
       if (!this.length) return;
       var index = this.indexOf(item);
+
       if (index > -1) {
         this.splice(index, 1);
         return this;
       }
     };
   }
+
   var listenList = [];
   var catcheList = [];
 
-  const isAlredyLoad = dom => {
+  var isAlredyLoad = function isAlredyLoad(dom) {
     if (catcheList.indexOf(dom) > -1) {
       return true;
     } else {
       return false;
     }
-  };
+  }; // 检测是否可以加载，如果可以则进行加载
 
-  // 检测是否可以加载，如果可以则进行加载
-  const isCanShow = ({ ele, ani }) => {
+  var isCanShow = function isCanShow(_ref) {
+    var ele = _ref.ele,
+      ani = _ref.ani;
     // var src = item.src;
     // 图片距离页面顶部的距离
-    let top = ele.getBoundingClientRect().top;
-    // 页面可视区域的高度
+    var top = ele.getBoundingClientRect().top; // 页面可视区域的高度
     // var windowHeight = window.innerHight;
     // top + 10 已经进入了可视区域10像素
+
     if (top + 10 < window.innerHeight) {
       // // 图片的懒加载
       // if (item.type === 1 || item.type === 2) {
@@ -54,28 +67,30 @@ export default (Vue, options = {}) => {
       // }
       // 盒模型的懒加载
       // if (item.type === 3) {
-      let dom = ele;
+      var dom = ele;
+
       if (dom.className.split(' ').indexOf(ani) === -1) {
         catcheList.push(dom);
         dom.classList.add(ani);
-      }
-      // }
+      } // }
+
       return true;
     } else {
       return false;
     }
   };
 
-  const onListenScroll = () => {
+  var onListenScroll = function onListenScroll() {
     window.addEventListener('scroll', function() {
       var length = listenList.length;
-      for (let i = 0; i < length; i++) {
+
+      for (var i = 0; i < length; i++) {
         isCanShow(listenList[i]);
       }
     });
-  };
-  // Vue 指令最终的方法
-  const addListener = (ele, binding) => {
+  }; // Vue 指令最终的方法
+
+  var addListener = function addListener(ele, binding) {
     // 绑定的图片地址
     // var imageSrc =
     //   Object.prototype.toString.call(binding.value) === '[object Object]'
@@ -85,7 +100,6 @@ export default (Vue, options = {}) => {
     //   Object.prototype.toString.call(binding.value) === '[object Object]'
     //     ? binding.value.type
     //     : 1;
-
     // 如果已经加载过，则无需重新加载，直接将src赋值
     if (isAlredyLoad(ele)) {
       return false;
@@ -94,20 +108,21 @@ export default (Vue, options = {}) => {
     var item = {
       ele: ele,
       ani: binding.value ? binding.value : 'animate-slideInUpSpe'
-    };
-    // 再看看是否可以显示此图片
+    }; // 再看看是否可以显示此图片
+
     if (isCanShow(item)) {
       return;
-    }
-    // 否则将dom地址和元素均放入监听的lisenList里
-    listenList.push(item);
+    } // 否则将dom地址和元素均放入监听的lisenList里
 
-    // 然后开始监听页面scroll事件
+    listenList.push(item); // 然后开始监听页面scroll事件
+
     onListenScroll();
   };
+
   Vue.directive('lazyAnimate', {
-    inserted: addListener
-    // updated: addListener,
+    inserted: addListener // updated: addListener,
     // componentUpdated: addListener
   });
 };
+
+exports.default = _default;
